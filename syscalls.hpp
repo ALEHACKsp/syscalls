34938a892f9717c37f678147082a4a094e871ad9
xo1337 <68725291+xo1337@users.noreply.github.com>
2021-10-02 20:19:00 -0600

    Create syscalls.hpp

---
+++ syscalls.hpp
@@ -0,0 +1,53 @@
+#include <Windows.h>
+
+template<class T>
+class Calls  {
+private:
+	void* _Function;
+	const char* _Name;
+public:
+	Calls(const char* _NameT) {
+		if (_NameT) {
+			this->_Name = _NameT;
+			this->_Function = reinterpret_cast<void*>(GetProcAddress(GetModuleHandleA("kernel32.dll"), _Name));
+
+			if (!_Function) {
+				throw std::exception("_Function returned NULL");
+			}
+		}
+	}
+
+	Calls(const char* _moduleName, const char* _NameT) {
+		if (_moduleName && _NameT) {
+			this->_Name = _NameT;
+			this->_Function = reinterpret_cast<void*>(GetProcAddress(GetModuleHandleA(_moduleName), _Name));
+
+			if (!_Function) {
+				throw std::exception("_Function returned NULL");
+			}
+		}
+	}
+
+	__forceinline const char* GetName() const {
+		return _Name;
+	}
+
+	__forceinline void* GetAddress() const {
+		return _Function;
+	}
+
+	__forceinline const char* AddressToString() {
+		char buf[64];
+		sprintf_s(buf, "0x%llx", GetAddress());
+		return buf;
+	}
+
+	template<typename ... Args> 
+	__forceinline T Run(Args ... args)
+	{
+		return reinterpret_cast<T(*)(Args ...)>(_Function)(args...);
+	}
+};
+
+#define SYSCALL(TYPE, NAME, ...) Calls<TYPE>("ntdll.dll", NAME).Run(__VA_ARGS__)
+#define USERCALL(TYPE, NAME, ...) Calls<TYPE>("kernel32.dll", NAME).Run(__VA_ARGS__)
